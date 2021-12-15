8fbe668f8ede7b73380cbd6d6d662faf7363b951
sooof <sooof@me.com>
2021-12-14 21:22:33 -0800

    Logout: axios.post &&  push work

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import axios from 'axios';
 
-const Logout = () => {
-    return (
-        <div>
-           <h1>Logout</h1> 
-        </div>
-    )
+const Logout = (props)=> {
+
+    useEffect(()=> {
+        // const token = localStorage.getItem("token");
+        axios.post('http://localhost:9000/api/logout', {}, {
+            headers:{
+                authorization: localStorage.getItem('token')
+            }
+        }).then(resp => {
+                localStorage.removeItem('token');
+                props.history.push('/login');
+            });
+    }, []);
+    return(<div></div>);
 }
+
 export default Logout;
